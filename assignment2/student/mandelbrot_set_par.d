ebb74d19a9034ec8155263eda37b42a9193e3555
Yumin Sun <suny@atbeetz19.informatik.tu-muenchen.de>
2018-04-24 15:52:17 +0200

    new

---
+++ assignment2/student/mandelbrot_set_par.d
@@ -0,0 +1,4 @@
+student/mandelbrot_set_par.o: student/mandelbrot_set_par.c \
+ /usr/stud/suny/Documents/parpro/assignment2/mandelbrot_set.h
+
+/usr/stud/suny/Documents/parpro/assignment2/mandelbrot_set.h:
