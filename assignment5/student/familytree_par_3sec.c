02092aac9a3f64742ac47a7bb2550dfc0b323454
Yumin Sun <ga45vab@mytum.de>
2018-05-09 23:35:57 +0200

    check correctness

---
+++ assignment5/student/familytree_par_3sec.c
@@ -0,0 +1,31 @@
+#include "familytree.h"
+#include <omp.h>
+
+void traverse(tree *node, int numThreads){
+    omp_set_nested(1);
+    omp_set_max_active_levels(4);//tested best
+
+	#pragma omp parallel
+	{
+//        if(node != NULL){
+//            node->IQ = compute_IQ(node->data);
+//            genius[node->id] = node->IQ;
+        
+            #pragma omp sections
+            {
+                #pragma omp section
+                {
+                    node->IQ = compute_IQ(node->data);
+                    genius[node->id] = node->IQ;
+                }
+                
+                #pragma omp section
+                {traverse(node->right, numThreads);}
+                
+                #pragma omp section
+                {traverse(node->left, numThreads);}
+            }
+//		}
+	}
+}
+
