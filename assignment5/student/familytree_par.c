69cd7b62bdfe8291b30543b385a77e1aec967307
Yumin Sun <suny@atbeetz19.informatik.tu-muenchen.de>
2018-05-09 20:28:10 +0200

    speed up 1.9

---
+++ assignment5/student/familytree_par.c
@@ -0,0 +1,31 @@
+#include "familytree.h"
+#include <omp.h>
+
+
+void traverse(tree *node, int numThreads){
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			if(node != NULL){
+				node->IQ = compute_IQ(node->data);
+                	        genius[node->id] = node->IQ;
+                	
+	
+        			#pragma omp task// firstprivate(numThreads) shared(node)//firstprivate(node,numThreads)
+        			{
+//                              printf("hello1");
+                			traverse(node->right, numThreads);
+        			}
+
+        			#pragma omp task// firstprivate(numThreads) shared(node)//firstprivate(node,numThreads)
+       				{
+//                              printf("hello2");
+                			traverse(node->left, numThreads);
+        			}
+			}
+		}
+	}
+
+}
+
