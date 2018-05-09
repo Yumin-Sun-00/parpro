de215d9d4c6ed7fc0438ffa703819d4e1536ffef
Yumin Sun <ga45vab@mytum.de>
2018-05-09 19:41:30 +0200

    code -11

--- assignment5/familytree_par.c
+++ assignment5/familytree_par.c
@@ -3,28 +3,23 @@
 
 void traverse(tree *node, int numThreads){
 	
-	#pragma omp parallel firstprivate(numThreads,node)
-	{
-		#pragma omp single
-		{
 			if(node != NULL){
 				node->IQ = compute_IQ(node->data);
 				genius[node->id] = node->IQ;
 			}
 
-			#pragma omp task firstprivate(numThreads)
+			#pragma omp task firstprivate(node,numThreads)
 			{		
 				printf("hello1");
 				traverse(node->right, numThreads);
 			}
 
-			#pragma omp task firstprivate(numThreads)
+			#pragma omp task firstprivate(node,numThreads)
 			{
 				printf("hello2");
 				traverse(node->left, numThreads);
 			}
-		}
-	}
+		
 
 }
 
