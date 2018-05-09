746ddf974f118655976932096fbae1e6d6924d2b
Yumin Sun <suny@atbeetz19.informatik.tu-muenchen.de>
2018-05-09 23:38:48 +0200

    should be work

--- assignment5/student/familytree_par.c
+++ assignment5/student/familytree_par.c
@@ -1,29 +1,31 @@
 #include "familytree.h"
 #include <omp.h>
 
-//#define T 30
-
 void traverse(tree *node, int numThreads){
-	omp_set_num_threads(numThreads);
-	
-    if(node != NULL){
-        node->IQ = compute_IQ(node->data);
-        genius[node->id] = node->IQ;
-				
-        #pragma omp parallel
-        {
-		    #pragma omp single
-            {
-                #pragma omp task final(node->id > 10*numThreads)
-                {
-                    traverse(node->right, numThreads);
-                }
+    omp_set_nested(1);
+    omp_set_max_active_levels(4);//tested best
 
-                #pragma omp task final(node->id > 10*numThreads)
+	#pragma omp parallel
+	{
+        if(node != NULL){
+//            node->IQ = compute_IQ(node->data);
+//            genius[node->id] = node->IQ;
+        
+            #pragma omp sections
+            {
+                #pragma omp section
                 {
-                    traverse(node->left, numThreads);
+                    node->IQ = compute_IQ(node->data);
+                    genius[node->id] = node->IQ;
                 }
+                
+                #pragma omp section
+                {traverse(node->right, numThreads);}
+                
+                #pragma omp section
+                {traverse(node->left, numThreads);}
             }
-        }
+	}
 	}
 }
+
