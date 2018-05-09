bd583a1669aa9e7a2d666ffd92c41fa30bbf40ff
Yumin Sun <ga45vab@mytum.de>
2018-05-09 21:27:45 +0200

    wierd,send email

--- assignment5/student/familytree_par.c
+++ assignment5/student/familytree_par.c
@@ -1,36 +1,29 @@
 #include "familytree.h"
 #include <omp.h>
 
-#define T 10
+//#define T 30
 
 void traverse(tree *node, int numThreads){
-	omp_set_num_threads( numThreads);
-	if(node != NULL){
-                                node->IQ = compute_IQ(node->data);
-                                genius[node->id] = node->IQ;
-				
-	#pragma omp parallel
-	{
-		#pragma omp single
-		{
-		//	if(node != NULL){
-		//		node->IQ = compute_IQ(node->data);
-                //	        genius[node->id] = node->IQ;
-                	
+	omp_set_num_threads(numThreads);
 	
-        			#pragma omp task final(node->id > T*numThreads)// firstprivate(numThreads) shared(node)//firstprivate(node,numThreads)
-        			{
-//                              printf("hello1");
-                			traverse(node->right, numThreads);
-        			}
+    if(node != NULL){
+        node->IQ = compute_IQ(node->data);
+        genius[node->id] = node->IQ;
+				
+        #pragma omp parallel
+        {
+		    #pragma omp single
+            {
+                #pragma omp task final(node->id > 10*numThreads)
+                {
+                    traverse(node->right, numThreads);
+                }
 
-        			#pragma omp task final(node->id > T*numThreads)// firstprivate(numThreads) shared(node)//firstprivate(node,numThreads)
-       				{
-//                              printf("hello2");
-                			traverse(node->left, numThreads);
-        			}
-		//	}
-		}
-	}
+                #pragma omp task final(node->id > 10*numThreads)
+                {
+                    traverse(node->left, numThreads);
+                }
+            }
+        }
 	}
 }
--- assignment5/student/familytree_par_sections4.c
+++ assignment5/student/familytree_par_sections4.c
@@ -1,50 +1,30 @@
 #include "familytree.h"
 #include <omp.h>
 
-
-//static int curr_depth = 0;
-
 void traverse(tree *node, int numThreads){
 
-omp_set_nested(1);
-omp_set_max_active_levels(4);//tested best
+    omp_set_nested(1);
+    omp_set_max_active_levels(4);//tested best
 
 	#pragma omp parallel
 	{
 		if(node != NULL){
 			node->IQ = compute_IQ(node->data);
-                        genius[node->id] = node->IQ;
+            genius[node->id] = node->IQ;
                 
-		#pragma omp sections
-		{
-			#pragma omp section
-			{
-				//printf("hello");
-				// if(node != NULL){
-				// 	node->IQ = compute_IQ(node->data);
-				// 	genius[node->id] = node->IQ;
-				// }
-				
-				// //curr_depth = curr_depth+1;
-				// printf("new section");
-				traverse(node->right, numThreads);
+            #pragma omp sections
+            {
+                #pragma omp section
+                {
+                    traverse(node->right, numThreads);
 
-			}
-			#pragma omp section
-			{
-				//printf("hello");
-				// if(node != NULL){
-				// 	node->IQ = compute_IQ(node->data);
-				// 	genius[node->id] = node->IQ;
-				// }
-				// //curr_depth = curr_depth+1;
-				// //printf("%d,",curr_depth);
-				// printf("new section");
-				 traverse(node->left, numThreads);
-			}
-		}
+                }
+                #pragma omp section
+                {
+                     traverse(node->left, numThreads);
+                }
+            }
 		}
 	}
-
 }
 
