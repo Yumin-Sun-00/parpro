696adb2caf16b9f2aa4bb60485f3bf1a3c6af61e
Yumin Sun <suny@atcremers10.informatik.tu-muenchen.de>
2018-05-18 09:41:41 +0200

    pass

--- assignment6/student/laplace_par.h
+++ assignment6/student/laplace_par.h
@@ -6,6 +6,7 @@
 template<int SIZE>
 inline void initialize(double a[SIZE + 2][SIZE + 2], double b[SIZE + 2][SIZE + 2])
 {
+    #pragma omp parallel for
     for (int i = 0; i < SIZE + 2; i++)
         for (int j = 0; j < SIZE + 2; j++)
         {
@@ -13,25 +14,25 @@ inline void initialize(double a[SIZE + 2][SIZE + 2], double b[SIZE + 2][SIZE + 2
             b[i][j] = 0.0;
         }
 
-
-
 }
 
 template<int SIZE>
 inline void time_step(double a[SIZE + 2][SIZE + 2], double b[SIZE + 2][SIZE + 2], int n)
 {
 
-
-	//TODO implement your solution in here
     if (n % 2 == 0)
     {
+	#pragma omp parallel for
         for (int i = 1; i < SIZE + 1; i++)
+	    
             for (int j = 1; j < SIZE + 1; j++)
                 b[i][j] = (a[i + 1][j] + a[i - 1][j] + a[i][j - 1] + a[i][j + 1]) / 4.0;
     }
     else
     {
+	#pragma omp parallel for
         for (int i = 1; i < SIZE + 1; i++)
+	    
             for (int j = 1; j < SIZE + 1; j++)
                 a[i][j] = (b[i + 1][j] + b[i - 1][j] + b[i][j - 1] + b[i][j + 1]) / 4.0;
     }
