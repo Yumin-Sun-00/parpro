0593d228fa9a734cbde2890ae275e7b2c4a6d7f5
Yumin Sun <ga45vab@mytum.de>
2018-05-31 19:29:17 +0200

    working

--- assignment9/student/reverse_par.c
+++ assignment9/student/reverse_par.c
@@ -3,20 +3,25 @@
 #include <string.h>
 #include <mpi.h> 
 #include "helper.h"
+
   
 void reverse(char *str, int strlen)
 {
         // parallelize this function and make sure to call reverse_str()
         // on each processor to reverse the substring.
 
-        int np, rank;
+    int np, rank;
+    MPI_Init(str,&strlen);
+    MPI_Comm_size(MPI_COMM_WORLD, &np); // Number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Process id
+    
+    char* substr = str;
+    int substrlen = strlen / np;
 
-    MPI_Comm_size(MPI_COMM_WORLD, &np);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- 
     if (rank == 0)
     {
-        reverse_str(str, strlen);
+        reverse_str(subtr, substrlen);
     }
+    MPI_Finalize ();
 
 }
