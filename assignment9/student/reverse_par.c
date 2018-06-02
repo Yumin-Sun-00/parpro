29b6535d941fa8f4407b6368964850c1cce6a346
Yumin Sun <ga45vab@mytum.de>
2018-06-02 21:38:56 +0200

    fix indexing error

--- assignment9/student/reverse_par.c
+++ assignment9/student/reverse_par.c
@@ -10,27 +10,29 @@ void reverse(char *str, int strlen)
     int size, rank;
     MPI_Comm_rank( MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
-
-    int sublen = ceil(strlen / (size-1));
- 
-    char* starts[size-1];
-    for(int i = 0 ; i < size-1; i++ ){starts[i] = str + sublen * i;}
-
     static int FROM_ID = 0;
 
+    int sublen = ceil(strlen / (size-1));
+    static int restlen= sublen * (size-1) - strlen;
+
     // Distribute jobs using the first process
-    if( rank == 0 )
+    if ( rank == 0 )
     {
         for(int i = 0; i < size-1; i++)
         {
-            MPI_Send(&starts[i], sublen, MPI_CHAR, i+1, 0, MPI_COMM_WORLD);
+            MPI_Send(str + sublen * i, sublen, MPI_CHAR, i+1, 0, MPI_COMM_WORLD);
         }
     }
     // Recieve jobs from process 0 and reverse the sub-string
+    else if ( rank == size-1 && restlen != 0 )
+    {
+        MPI_Recv(str + sublen * (rank-1), restlen, MPI_CHAR, FROM_ID, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+        reverse_str(str + sublen * (rank-1), sublen);
+    }
     else
     {
-        MPI_Recv(&starts[rank-1], sublen, MPI_CHAR, FROM_ID, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-        reverse_str(starts[rank-1], sublen);
+        MPI_Recv(str + sublen * (rank-1), sublen, MPI_CHAR, FROM_ID, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+        reverse_str(str + sublen * (rank-1), sublen);
     }
     MPI_Finalize();
     return;
