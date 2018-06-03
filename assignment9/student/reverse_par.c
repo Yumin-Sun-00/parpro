b05432c16a8a1d2612e8255d49732df8ac9a0b85
Yumin Sun <ga45vab@mytum.de>
2018-06-03 17:35:49 +0200

    seq segmentation fault

--- assignment9/student/reverse_par.c
+++ assignment9/student/reverse_par.c
@@ -10,37 +10,34 @@ void reverse(char *str, int strlen)
     int gsize, rank;
     MPI_Comm_rank( MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &gsize);
-    static int FROM_ID = 0;
+    int stride = ceil(strlen / (gsize));
+
+    // MPI_Scatterv params
+    int ROOT = 0;
+    MPI_Datatype sendtype = MPI_CHAR;
+    MPI_Datatype recvtype = MPI_CHAR;
     
-    MPI_Comm comm;
-    int root, rbuf[100];
-    int sublen = ceil(strlen / (gsize));
-    int restlen= strlen - sublen * (gsize-1);
+    int *displs, *scounts; 
+    displs = (int *)malloc(gsize*sizeof(int)); 
+    scounts = (int *)malloc(gsize*sizeof(int));
+    for (int i=0; i < gsize; ++i) { 
+        displs[i] = i*stride; 
+        scounts[i] = stride; 
+    } 
+    int rest = strlen - stride * (gsize-1);
+    if (rest != 0) scounts[gsize-1] = rest;
 
-    MPI_Scatterv(str, &sublen, const int *displs,
-                 MPI_Datatype sendtype, void *recvbuf, int recvcount,
-                 MPI_Datatype recvtype,
-                 int root, MPI_Comm comm)
+    char rbufs[strlen];
+    
+    // Distribute jobs
+    MPI_Scatterv(str, scounts, displs, sendtype,  \
+                 rbufs, stride, recvtype,           \
+                 ROOT, MPI_COMM_WORLD);
 
-    // Distribute jobs using the first process
-    if ( rank == 0 )
-    {
-        for(int i = 0; i < size-1; i++)
-        {
-            MPI_Send(str + sublen * i, sublen, MPI_CHAR, i+1, 0, MPI_COMM_WORLD);
-        }
-    }
-    // Recieve jobs from process 0 and reverse the sub-string
-    else if ( rank == size-1 && restlen != 0 )
-    {
-        MPI_Recv(str + sublen * (rank-1), restlen, MPI_CHAR, FROM_ID, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-        reverse_str(str + sublen * (rank-1), restlen);
-    }
-    else
-    {
-        MPI_Recv(str + sublen * (rank-1), sublen, MPI_CHAR, FROM_ID, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-        reverse_str(str + sublen * (rank-1), sublen);
-    }
+    reverse_str(&rbufs[displs[rank]], scounts[rank]);
+    
     MPI_Finalize();
+    free(scounts);
+    free(displs);
     return;
 }
