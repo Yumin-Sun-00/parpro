8fee345c1e6291e2defd7eeacd532a8d9ab1b3fd
Yumin Sun <ga45vab@mytum.de>
2018-06-05 14:52:24 +0200

    send recieve

--- assignment9/student/reverse_par.c
+++ assignment9/student/reverse_par.c
@@ -7,17 +7,19 @@
 
 void reverse(char *str, int strlen)
 {
-    printf("strlen: %d", strlen);    
+    //printf("strlen: %d", strlen);
     int gsize, rank;
     MPI_Comm_rank( MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &gsize);
     int stride = ceil( (float) strlen / (float) gsize);
-    printf("stride %d\n",stride);
+    //printf("stride %d\n",stride);
+
     // MPI_Scatterv params
     int *displs, *scounts; 
     displs = (int *)malloc(gsize*sizeof(int)); 
     scounts = (int *)malloc(gsize*sizeof(int));
-    for (int i=0; i < gsize; ++i) { 
+    for (int i=0; i < gsize; ++i)
+    {
         displs[i] = i*stride; 
         scounts[i] = stride; 
     } 
@@ -41,23 +43,40 @@ void reverse(char *str, int strlen)
     /* print what each process received*/
     printf("Chunck from process %d is ",rank);
     print(rbufs, scounts[rank]);
+
     printf("rank %d:, displ: %d, scounts: %d, rev_displs: %d", rank, displs[rank],scounts[rank],rev_displs[rank]);
     printf("\n");
+
     reverse_str(rbufs, scounts[rank]);
-    printf("Reversed:");
-    printf("\n");
+
+    printf("Reversed: ");
     print(rbufs,scounts[rank]);
 
 
     if (rank == 0)
     {
-    
+        strncpy(&str[rev_displs[0]], rbufs, scounts[0]);
+        for(int i = 1; i < gsize; i++)
+        {
+            char recv[scounts[i]];
+            MPI_Revc(recv, scounts[i], MPI_CHAR, i, 16, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            strncpy(&str[rev_displs[i]], recv, scounts[i]);
+        }
+
+    }
+    else
+    {
+        for(int i = 1; i <gsize; i++)
+        {
+            MPI_Send(rbufs, scounts[i], MPI_CHAR, 0, 16, MPI_COMM_WORLD);
+        }
+
     }
 
-    for(int i = 0; i<scounts[rank]; i++)
-    {
-	str[rev_displs[rank]+i] = rbufs[i];
-    }    
+//    for(int i = 0; i<scounts[rank]; i++)
+//    {
+//	str[rev_displs[rank]+i] = rbufs[i];
+//    }
 //strncpy(&str[rev_displs[rank]], rbufs, scounts[rank]);    
     
    free(displs);
