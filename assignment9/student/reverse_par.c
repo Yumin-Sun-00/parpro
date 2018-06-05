31e07913a7300ef14c7d4ef9de8ad1e9f513ec4e
Yumin Sun <ga45vab@mytum.de>
2018-06-05 16:23:08 +0200

    miao

--- assignment9/student/reverse_par.c
+++ assignment9/student/reverse_par.c
@@ -10,29 +10,23 @@ void reverse(char *str, int strlen)
     int gsize, rank;
     MPI_Comm_rank( MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &gsize);
-    int stride = floor( (float) strlen / (float) gsize);
+    int stride = floor( (float) strlen / (float) (gsize));
 
     // MPI_Scatterv params
     int *displs,*scounts;
     displs = (int *)malloc(gsize*sizeof(int));
     scounts = (int *)malloc(gsize*sizeof(int));
-//    int *r_displ;
-//    r_displ = (int*) malloc ( gsize * sizeof (int) );
     for (int i=0; i < gsize; ++i)
     {
 	displs[i] = i*stride;   
         scounts[i] = stride;
-//        r_displ[i] = strlen-i*stride-stride;
     }
     int rest = strlen - stride * (gsize-1);
-    if (rest != 0)
-    {
-        scounts[gsize-1] = rest;
-//        r_displ[gsize-1] = 0;
-    }
+    if (rest != 0) scounts[gsize-1] = rest;
 
-
-    char rbufs[scounts[rank]];
+    //char rbufs[scounts[rank]];
+    char* rbufs;
+    rbufs  = (char*)malloc(scounts[rank]*sizeof(char));
 
     // Distribute jobs
     MPI_Scatterv(str, scounts, displs, MPI_CHAR,  \
@@ -49,12 +43,14 @@ void reverse(char *str, int strlen)
         {
             r_displ[i]=strlen-i*stride-scounts[i] ;
         }
-        strncpy(&str[r_displ[0]], rbufs, scounts[0]);
+        memcpy ( &str[r_displ[0]], rbufs, scounts[0] );
+        //strncpy(&str[r_displ[0]], rbufs, scounts[0]);
         for(int i = 1; i < gsize; i++)
         {
             char recv[scounts[i]];
             MPI_Recv(recv, scounts[i], MPI_CHAR, i, 16, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            strncpy(&str[r_displ[i]], recv, scounts[i]);
+            memcpy ( &str[r_displ[i]], rbufs, scounts[i] );
+            //strncpy(&str[r_displ[i]], recv, scounts[i]);
 	}
         free(r_displ);
     }
@@ -65,6 +61,5 @@ void reverse(char *str, int strlen)
 
    free(displs);
    free(scounts);
-   //free(r_displ);
    return;
 }
